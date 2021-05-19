c8af0acf23e5a394b03f16484e6183ffdb538f0a
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-05-19 19:44:34 +0100

    Fixed internal server error issue

--- server/src/main/scala/algebras/TweetAlg.scala
+++ server/src/main/scala/algebras/TweetAlg.scala
@@ -7,6 +7,7 @@ import org.http4s.{Header, Headers, Request, Uri}
 import org.http4s.client.Client
 import org.http4s.implicits.http4sLiteralsSyntax
 import utils.AppEntityEncodersAndDecoders._
+import cats.implicits._
 
 trait TweetAlg[F[_]] {
   def getSampleTweets: F[SampleTweet]
@@ -16,7 +17,17 @@ trait TweetAlg[F[_]] {
 object TweetAlg {
 
   def imp[F[_]: Sync](config: TwitterConfig, client: Client[F]): TweetAlg[F] = new TweetAlg[F] {
-    override def getSampleTweets: F[SampleTweet] = ???
+    override def getSampleTweets: F[SampleTweet] = {
+      Sync[F].fromEither(Uri.fromString(config.sampleStream.sampleStream)).flatMap { parsedUri =>
+        client.expect[SampleTweet](
+          Request[F](
+            uri = parsedUri,
+            headers =
+              Headers.of(Header("Authorization", s"Bearer ${config.bearersToken.bearersToken}"))
+          )
+        )
+      }
+    }
 
     override def getTodoTweets: F[Todo] =
       client.expect[Todo](uri"https://jsonplaceholder.typicode.com/todos/1")
